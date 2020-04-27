52be5a4e5bd74b2cec8409944fdbd566f92a7955
Patryk Janowski <pjanowski97@gmail.com>
2020-04-27 21:07:37 +0200

    changes in google api

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const OAuth2Data = require('./google_key.json');
 
 const app = express();
 
-const CLIENT_ID = OAuth2Data.web.client.id;
-const CLIENT_SECRET = OAuth2Data.web.client.secret;
-const REDIRECT_URL = OAuth2Data.web.client.redirect[0];
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 var authed = false;
