b77ba271e4f46cac2cc15edeba791bf335b66009
Patryk Janowski <pjanowski97@gmail.com>
2020-04-27 21:14:34 +0200

    changes in package to run 2

--- app.js
+++ app.js
@@ -56,5 +56,5 @@ app.get('/auth/google/callback', function (req, res) {
     }
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running at ${port}`));
